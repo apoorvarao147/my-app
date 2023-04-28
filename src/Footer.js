e8d7afe8478abf0d59e3d9eb6d92d47bee749c95
apoorvarao147 <131875462+apoorvarao147@users.noreply.github.com>
2023-04-28 14:06:53 +0530

    chane header, footer and add signup

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Gadha(){
+    return(
+        <div>
+            <h1>this is gadha footer</h1>
+        </div>
+    )
+}
+
+export default Gadha
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header className="App-header">
+        <h2>My Header</h2>
+        </header>
+    )
+}
+
+export default Header
